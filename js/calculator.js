f8833b67f796d6401865bf01c3dfe9776905e84c
OliviacMorgan <oliviamorgan20@gmail.com>
2021-03-26 16:58:44 -0400

    on and off hiding system for questionar estblished but a little clunky

--- js/calculator.js
+++ js/calculator.js
@@ -2,26 +2,27 @@
 
 window.addEventListener("load", e=>{
     var cardcost = document.getElementById("fiftycost").textContent;
-    var totalcountfield = document.getElementById("totalcount");
+    var totalcost = document.getElementById("total")
+    totalcost.innerHTML=(cardcost*(document.getElementById("count").value)).toFixed(2);
 
     //dynamically calculate current cost of cards based on order quantity
     document.getElementById("count").addEventListener("input", updateCost);
     function updateCost(e) {
-      let val = e.target.value
-      let newcardcost = cardcost;
+      document.getElementById("totalcount").innerHTML=e.target.value;
+      let val = e.target.value;
+      if (val < 100) {newcardcost = cardcost};
       if (val >= 100 && val <= 499) {
-        newcardcost=cardcost;
         newcardcost = (cardcost * .95).toFixed(2);
       }
       if (val >= 500 && val <= 999) {
-        newcardcost=cardcost;
         newcardcost = (cardcost * .9).toFixed(2);
       }
       if (val >= 1000) {
-        newcardcost=cardcost;
         newcardcost = (cardcost * .85).toFixed(2);
       }
       document.getElementById("curcost").textContent = newcardcost;
+
+      totalcost.innerHTML = (newcardcost*val).toFixed(2);;
     }
     
 
