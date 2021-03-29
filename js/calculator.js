0fe101f0252ffabe9da8b8499df6c608a5288d76
OliviacMorgan <oliviamorgan20@gmail.com>
2021-03-29 09:22:20 -0400

    Calculator cleaned up and working

--- js/calculator.js
+++ js/calculator.js
@@ -1,28 +1,68 @@
 
-
 window.addEventListener("load", e=>{
-    var cardcost = document.getElementById("fiftycost").textContent;
-    var totalcost = document.getElementById("total")
-    totalcost.innerHTML=(cardcost*(document.getElementById("count").value)).toFixed(2);
+  var cardcost = document.getElementById("fiftycost").textContent;
+    cardtotal = document.getElementById("count");
+    totalelem = document.getElementById("total");
+    costelem = document.getElementById("curcost");
 
+    totalelem.value = (cardcost*(cardtotal.value)).toFixed(2);
+    costelem.value = cardcost
+  
     //dynamically calculate current cost of cards based on order quantity
-    document.getElementById("count").addEventListener("input", updateCost);
-    function updateCost(e) {
-      document.getElementById("totalcount").innerHTML=e.target.value;
-      let val = e.target.value;
+    document.querySelector("form").addEventListener("input", updateCost);
+
+    function updateCost() {
+      let val = cardtotal.value;
+          
+      document.getElementById("totalcount").value=val;
+
       if (val < 100) {newcardcost = cardcost};
       if (val >= 100 && val <= 499) {
-        newcardcost = (cardcost * .95).toFixed(2);
+        newcardcost = (cardcost * .95);
       }
       if (val >= 500 && val <= 999) {
-        newcardcost = (cardcost * .9).toFixed(2);
+        newcardcost = (cardcost * .9);
       }
       if (val >= 1000) {
-        newcardcost = (cardcost * .85).toFixed(2);
+        newcardcost = (cardcost * .85);
       }
-      document.getElementById("curcost").textContent = newcardcost;
+      costper = newcardcost;
 
-      totalcost.innerHTML = (newcardcost*val).toFixed(2);;
+      //total cost based on dynamic 
+      totalamt = (newcardcost*val);
+      costper = parseFloat(newcardcost)
+
+      //if you select not card attached
+
+      if (document.querySelector("input[type='radio'].no-card").checked){
+        totalamt -= (val*5);
+        costper-= 5;
+      };
+      if (document.querySelector("#yes-envelopes").checked){
+        totalamt += (val*.5)
+        costper+= .5;
+      }
+      if (document.querySelector("#yes-pack").checked){
+        totalamt += (val*.5)
+        costper += .5;
+      }
+      if (document.querySelector("#yes-returnadd").checked){
+        totalamt += (val*.3)
+        costper += .3;
+      }
+      if (document.querySelector("#yes-clients").checked){
+        totalamt += (val*1)
+        costper += 1;
+      }
+      if (document.querySelector("#yes-mail").checked){
+        totalamt += (val*1)
+        costper += 1;
+      }
+
+      totalelem.value = totalamt.toFixed(2);
+      costelem.value = costper.toFixed(2);
+
+
     }
     
 
@@ -32,8 +72,6 @@ window.addEventListener("load", e=>{
     document.getElementById('onethousandcost').innerHTML = (cardcost*.85).toFixed(2);
 
     //read out order quantity in product panel
-
-    
     document.getElementById("popupform").addEventListener("input", updatetotal);
     
     function updatetotal(){
--- js/form_visual.js
+++ js/form_visual.js
@@ -2,25 +2,17 @@
 window.addEventListener("load", e=>{
 
     $("input[type='radio'].data-card").change(function() {
-      //Check the value for "paypal", also verify that the radio is checked
       if (this.checked) {
-          $(".bookinput").hide();
           $(".cardinput").show();
       } else {
-          //I figured you would want to unhide these if paypal isnt checked, here's the else
-          $(".bookinput").show();
           $(".cardinput").hide();
       }
   }); 
-  $("input[type='radio'].data-book").change(function() {
-      //Check the value for "paypal", also verify that the radio is checked
+  $("input[type='radio'].no-card").change(function() {
       if (this.checked) {
-          $(".bookinput").show();
           $(".cardinput").hide();
       } else {
-          //I figured you would want to unhide these if paypal isnt checked, here's the else
           $(".cardinput").show();
-          $(".bookinput").hide();
       }
   })
 });
